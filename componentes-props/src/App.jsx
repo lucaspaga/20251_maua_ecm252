be3f95b54c49d79056d01f07b814022300898610
LUCAS CASTANHO PAGANOTTO CARVALHO <22.00921-3@maua.br>
2025-03-15 11:06:58 -0300

    learn(04-react): encerra a apostila

--- componentes-props/src/App.jsx
+++ componentes-props/src/App.jsx
@@ -1,44 +1,74 @@
 import Pedido from "./Pedido"
+import Cartao from "./Cartao"
+import FeedBack from "./FeedBack"
+
 const App = () => {
+  const textoOK = 'Já chegou'
+  const textoNOK = 'Nãp chegou ainda'
+  const funcaoOK = () => alert('Agradecemos a confirmação')
+  const funcaoNOK = () => alert('Verificamos o ocorrido')
+  const componenteFeedback = (
+    <FeedBack
+      textoOK={textoOK}
+      textoNOK={textoNOK}
+      funcaoOK={funcaoOK}
+      funcaoNOK={funcaoNOK}
+    />
+  )
   return (
     <div className="container border mt-2">
       <div className="row">
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
+        <div className="col-lg-6 col-xxl-4">
           {/* .card>(.card-header+.card-body)*/}
 
-          <Pedido 
-            data = "22/02/2024"
-            titulo = "SSD"
-            descricao="Um SSD de 512Gb"
-            icone="hdd"/>
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido
+              titulo="SSD"
+              descricao="Um SSD de 512Gb"
+              icone="hdd" />
+              {componenteFeedback}
+          </Cartao>
 
         </div>
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
+        <div className="col-lg-6 col-xxl-4">
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido
+              titulo="Livro"
+              descricao="Harry Potter"
+              icone="book" />
+              {componenteFeedback}
+          </Cartao>
 
-          <Pedido 
-            data = "22/02/2024"
-            titulo = "Livro"
-            descricao="Harry Potter"
-            icone="book"/>
-            
 
-        </div>
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
-
-          <Pedido 
-            data = "22/02/2024"
-            titulo = "Hipopótamo"
-            descricao="Sim, um hipo!"
-            icone="hippo"/>
 
         </div>
-        <div className="col-sm-12 col-lg-6 col-xxl-3">
+        <div className="col-lg-6 col-xxl-4">
 
-          <Pedido 
-            data = "22/02/2024"
-            titulo = "Boneco de Neve"
-            descricao="Um boneco de neve que balança sozinho"
-            icone="snowman fa-shake"/>
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido
+              data="22/02/2024"
+              titulo="Hipopótamo"
+              descricao="Sim, um hipo!"
+              icone="hippo" />
+              {componenteFeedback}
+          </Cartao>
+
+
+        </div>
+        <div className="col-lg-6 col-xxl-4">
+          <Cartao
+            cabecalho={new Date().toDateString()}>
+            <Pedido
+              data="22/02/2024"
+              titulo="Boneco de Neve"
+              descricao="Um boneco de neve que balança sozinho"
+              icone="snowman fa-shake" />
+              {componenteFeedback}
+          </Cartao>
+
 
         </div>
 
