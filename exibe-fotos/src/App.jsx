384419a9028bb25df337e00719f56fc946054180
LUCAS CASTANHO PAGANOTTO CARVALHO <22.00921-3@maua.br>
2025-04-05 12:51:50 -0300

    learn(07-react): encerra a pagina 8

---
+++ exibe-fotos/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Busca from './components/Busca'
+
+const App = () => {
+  return (
+
+    <div className="grid border-1 justify-content-center w-9 m-auto">
+      <div className="col-12 text-center">
+        <h4>Exibir uma lista de...</h4>
+      </div>
+      <Busca />
+    </div>
+
+  )
+}
+
+export default App
