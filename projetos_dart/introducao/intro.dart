753a24757ae214a0d89f01a21e6f55f31efcc2dd
LUCAS CASTANHO PAGANOTTO CARVALHO <22.00921-3@maua.br>
2025-08-16 11:45:07 -0300

    learn: finaliza apostila 1

---
+++ projetos_dart/introducao/intro.dart
@@ -0,0 +1,26 @@
+/// essa é a função main
+void main(){
+    double real = 2.7;
+    int inteiro = real.round();
+    print(inteiro);
+
+
+    // String idadeTextual = '25';
+    // int idade = int.parse(idadeTextual);
+    // String pesoTextual = '80.5';
+    // double peso = double.parse(pesoTextual);
+    // String alturaTextual = '1.80';
+    // num altura = num.parse(alturaTextual);
+    // print(altura.runtimeType);
+
+    // print("Hello, World");
+    // String nome = 'João';
+    // bool deMaior = false;
+    // String sobrenome = 'Silva';
+    // String nomeCompleto = '$nome $sobrenome';
+    // int idade = 17;
+    // print('Ano que vem terei ${idade + 1}');
+    // num peso = 80.7;
+    // double altura = 1.82;
+
+}
